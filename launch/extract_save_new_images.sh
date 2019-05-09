91aaef3fe730046bd66c6a4e790e163e8c5aed33
Sevim Caliskan <csevim@zinc.ee.ethz.ch>
2019-05-09 20:21:47 +0200

    minor

--- launch/extract_save_new_images.sh
+++ launch/extract_save_new_images.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-export SGE_GPU_ALL="$(ls -rt /tmp/lock-gpu*/info.txt | xargs grep -h $(whoami) | awk '{print $2}' | paste -sd "," -)"
-export SGE_GPU=$(echo $SGE_GPU_ALL |rev|cut -d, -f1|rev) # USE LAST GPU by request time.
-echo "SGE gpu=$SGE_GPU allocated in this use"
-
-CUDA_VISIBLE_DEVICES=$SGE_GPU python extract_images.py \
+python extract_images.py \
 --data_dir /srv/glusterfs/csevim/datasets/affectnet \
 --test_images_folder cropped2 \
 --output_dir /srv/glusterfs/csevim/datasets/fid_test/from_network_with_labels \
@@ -17,7 +13,7 @@ CUDA_VISIBLE_DEVICES=$SGE_GPU python extract_images.py \
 --emo_test_file /srv/glusterfs/csevim/datasets/affectnet/affectnet_emos.pkl
 
 
-CUDA_VISIBLE_DEVICES=$SGE_GPU python extract_images.py \
+python extract_images.py \
 --data_dir /srv/glusterfs/csevim/datasets/affectnet \
 --test_images_folder cropped2 \
 --output_dir /srv/glusterfs/csevim/datasets/fid_test/from_network_4d \
