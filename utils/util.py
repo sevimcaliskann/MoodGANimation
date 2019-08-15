4105e95e5fa1e3ec352ebd080a6875d81d90ec12
sevim <sevimcaliskansc@gmail.com>
2019-08-15 19:44:05 +0200

    debugging

--- utils/util.py
+++ utils/util.py
@@ -9,7 +9,7 @@ import cv2
 
 def tensor2im(img, imtype=np.uint8, unnormalize=True, idx=0, nrows=None):
     # select a sample or create grid if img is a batch
-    if len(img.shape) == 4:
+    if len(img.shape) >= 3:
         nrows = nrows if nrows is not None else int(math.sqrt(img.size(0))*math.sqrt(img.size(1)/3.0))
         img = img[idx] if idx >= 0 else torchvision.utils.make_grid(img, nrows)
 
