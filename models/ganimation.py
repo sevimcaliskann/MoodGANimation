31a34f242325c270d6c96fee4505afbefb5672b6
sevim <sevimcaliskansc@gmail.com>
2019-08-28 22:45:50 +0200

    both tensors are on cuda, checking division operator on conditional loss of generator

--- models/ganimation.py
+++ models/ganimation.py
@@ -239,10 +239,11 @@ class GANimation(BaseModel):
         d_fake_desired_img_masked_prob, d_fake_desired_img_masked_cond = self._D.forward(fake_imgs_masked)
 
         self._loss_g_masked_fake = self._compute_loss_D(d_fake_desired_img_masked_prob, True) * self._opt.lambda_D_prob
+        start = time.time()
         #self._loss_g_masked_cond = self._criterion_D_cond(d_fake_desired_img_masked_cond, self._desired_cond) / self._B * self._opt.lambda_D_cond
-        self._loss_g_masked_cond = self._criterion_D_cond(d_fake_desired_img_masked_cond, self._desired_cond) / self._B * self._opt.lambda_D_cond        # G(G(Ic1,c2), c1)
-        print('d_fake_desired_img_masked_cond: ', d_fake_desired_img_masked_cond)
-        print('self._desired_cond: ', self._desired_cond)
+        #self._loss_g_masked_cond = self._criterion_D_cond(d_fake_desired_img_masked_cond, self._desired_cond) / self._B * self._opt.lambda_D_cond        # G(G(Ic1,c2), c1)
+        self._loss_g_masked_cond = self._criterion_D_cond(d_fake_desired_img_masked_cond, self._desired_cond) * self._opt.lambda_D_cond
+        print('time passed: ', time.time()-start)
 
         rec_real_img_rgb, rec_real_img_mask = self._G.forward(fake_imgs_masked, self._real_cond)
         rec_real_img_mask = self._do_if_necessary_saturate_mask(rec_real_img_mask, saturate=self._opt.do_saturate_mask)
