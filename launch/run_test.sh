0d82a9c3daa2a9676bd74dbcd020ff278c3add80
sevim <sevimcaliskansc@gmail.com>
2019-09-15 00:13:35 +0200

    back to recurrent way of training

--- launch/run_test.sh
+++ launch/run_test.sh
@@ -4,10 +4,10 @@ python test.py \
 --data_dir /srv/glusterfs/csevim/datasets/affectnet \
 --training_aus_file /srv/glusterfs/csevim/datasets/affectnet/aus_affectnet.pkl \
 --test_aus_file /srv/glusterfs/csevim/datasets/affectnet/aus_affectnet.pkl \
---input_path /home/sevim/Downloads/faces/imgs/face4.jpg \
+--input_path /home/sevim/Downloads/faces/imgs/face.png \
 --output_dir /home/sevim/Downloads/master_thesis_study_documents/code-examples/GANimation/test_outputs \
 --checkpoints_dir /home/sevim/Downloads/master_thesis_study_documents/code-examples/ganimation_checkpoints \
---name affwild_per_frame_5frames \
+--name ganimation_with_5frames \
 --cond_nc 2 \
 --load_epoch -1 \
 --frames_cnt 10
