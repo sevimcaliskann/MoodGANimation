154005e72016f28bca41407baada94165652be60
sevim <sevimcaliskansc@gmail.com>
2019-10-09 14:01:40 +0200

    debugging

--- models/ganimation.py
+++ models/ganimation.py
@@ -57,7 +57,7 @@ class GANimation(BaseModel):
         self._current_lr_D = self._opt.lr_D
 
         # initialize optimizers
-        print('original size: ', len(self._G.parameters()))
+        print('original size: ', len(list(self._G.parameters())))
         self._optimizer_G = torch.optim.Adam(self._G.get_parameters(), lr=self._current_lr_G,
                                              betas=[self._opt.G_adam_b1, self._opt.G_adam_b2])
         self._optimizer_D = torch.optim.Adam(self._D.parameters(), lr=self._current_lr_D,
