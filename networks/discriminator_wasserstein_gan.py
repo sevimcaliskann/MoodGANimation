1d43147fea6d2df875e84fad13ba45a54bfc2f01
sevim <sevimcaliskansc@gmail.com>
2019-10-09 12:49:16 +0200

    debugging

--- networks/discriminator_wasserstein_gan.py
+++ networks/discriminator_wasserstein_gan.py
@@ -23,6 +23,7 @@ class Discriminator(NetworkBase):
         self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=k_size, bias=False)
 
     def forward(self, x, feats=None):
+        print('x type: ', type(x))
         h = self.feat_layers[0](x)
         print('before h type: ', type(h))
         next_feats = dict()
