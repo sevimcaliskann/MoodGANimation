86f92815809ebb14fd21f1516598ebb599d1c241
sevim <sevimcaliskansc@gmail.com>
2019-10-13 00:09:25 +0200

    adding recurrent connection in the last fc layer of discriminator

--- networks/discriminator_wasserstein_gan.py
+++ networks/discriminator_wasserstein_gan.py
@@ -9,40 +9,36 @@ class Discriminator(NetworkBase):
         super(Discriminator, self).__init__()
         self._name = 'discriminator_wgan'
 
-        self.feat_layers = []
-        self.feat_layers.append(nn.Sequential(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1), \
-                                         nn.LeakyReLU(0.01, inplace=True)))
+        feat_layers = []
+        feat_layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
+        feat_layers.append(nn.LeakyReLU(0.01, inplace=True))
 
         curr_dim = conv_dim
         for i in range(1, repeat_num):
-            if i <=repeat_num/2:
-                self.feat_layers.append(nn.Sequential(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1), \
-                                             nn.LeakyReLU(0.01, inplace=True)))
-            else:
-                self.feat_layers.append(nn.Sequential(nn.Conv2d(2*curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1), \
-                                             nn.LeakyReLU(0.01, inplace=True)))
+            feat_layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1))
+            feat_layers.append(nn.LeakyReLU(0.01, inplace=True))
+            #if i <=repeat_num/2:
+            #    self.feat_layers.append(nn.Sequential(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1), \
+            #                                 nn.LeakyReLU(0.01, inplace=True)))
+            #else:
+            #    self.feat_layers.append(nn.Sequential(nn.Conv2d(2*curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1), \
+            #                                 nn.LeakyReLU(0.01, inplace=True)))
 
             curr_dim = curr_dim * 2
 
         k_size = int(image_size / np.power(2, repeat_num))
+        self.main = nn.Sequential(*feat_layers)
         self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
         self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=k_size, bias=False)
 
     def forward(self, x, feats=None):
-        h = self.feat_layers[0](x)
-        next_feats = dict()
-        for idx in range(1, len(self.feat_layers)):
-            if idx<= len(self.feat_layers)/2:
-                h = self.feat_layers[idx](h)
-            else:
-                next_feats[idx] = h
-                h_cat = torch.cat([h, feats[idx]], dim=1) if feats is not None \
-                    else torch.cat([h, torch.randn(h.size()).cuda()], dim=1)
-                h = self.feat_layers[idx](h_cat)
+        h = self.main(x)
+        h_cat = torch.cat([h, feats], dim=1) if feats is not None \
+            else torch.cat([h, torch.randn(h.size()).cuda()], dim=1)
 
-        out_real = self.conv1(h)
-        out_aux = self.conv2(h)
-        return (out_real.squeeze(), out_aux.squeeze(), next_feats)
+        out_real = self.conv1(h_cat)
+        out_aux = self.conv2(h_cat)
+        return (out_real.squeeze(), out_aux.squeeze(), h)
 
     def carry_to_cuda(self):
         for layer in self.feat_layers:
