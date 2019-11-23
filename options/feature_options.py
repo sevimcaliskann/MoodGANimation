43cffe8414cec856e9745b27b473e66208c0a85b
sevim <sevimcaliskansc@gmail.com>
2019-11-24 01:49:19 +0300

    cleaning and finalizing this branch

--- options/feature_options.py
+++ options/feature_options.py
@@ -6,7 +6,7 @@ class FeatureOptions(BaseOptions):
         BaseOptions.initialize(self)
         #self._parser.add_argument('--input_path', type=str, help='path to image')
         self._parser.add_argument('--output_dir', type=str, default='./output', help='output path')
-	self._parser.add_argument('--images_folder', type=str, default='imgs', help='images folder')
+        self._parser.add_argument('--images_folder', type=str, default='imgs', help='images folder')
         self._parser.add_argument('--output_file', type=str, default='./output', help='output file to keep the features')
         self._parser.add_argument('--input_file', type=str, default='./output', help='the path for the file which keeps the names of images')
         self._parser.add_argument('--input_folder', type=str, default='./output', help='folder where images are stored')
